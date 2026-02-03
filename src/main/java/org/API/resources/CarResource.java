0fb09a4dc315958c056b59d3977c5987b4cd2ff0
Robert <boren534@gmail.com>
2026-02-03 15:58:33 +0100

    Byggde grund till CarResources *Skapa koppling mellan repo och Resources *Testade Get Med Postman

--- src/main/java/org/API/resources/CarResource.java
+++ src/main/java/org/API/resources/CarResource.java
@@ -1,23 +1,32 @@
+package org.API.resources;
+
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.MediaType;
 import java.util.List;
 
 import org.API.entities.CarEntity;
+import org.API.repositories.CarRepository;
+
 
 @Path("api/car")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class CarResource {
+
+    @Inject
+    CarRepository carRepository;
     
     @GET
     public Response getCars() {
-        List<CarEntity> cars = CarRepository.getAllCars();
+        List<CarEntity> cars = carRepository.getAllCars();
 
         if(cars.isEmpty()) {
-            return Response.status(Response.Status.NO_CONTENT).build();
+            return Response.noContent().build();
         }
         return Response.ok(cars).build();
     }

90e46d256c2e053666d4066dd0bda83bcea1b755
Robert <boren534@gmail.com>
2026-02-03 15:59:03 +0100

    Kommenterade ut för att testa app

--- src/main/java/org/API/repositories/UserRepository.java
+++ src/main/java/org/API/repositories/UserRepository.java
@@ -1,29 +1,29 @@
-import jakarta.enterprise.context.ApplicationScoped;  // @ApplicationScoped
-import jakarta.persistence.EntityManager;             // EntityManager
-import jakarta.persistence.PersistenceContext;        // @PersistenceContext
+// import jakarta.enterprise.context.ApplicationScoped;  // @ApplicationScoped
+// import jakarta.persistence.EntityManager;             // EntityManager
+// import jakarta.persistence.PersistenceContext;        // @PersistenceContext
 
-@ApplicationScoped
-public class UserRepository {
+// @ApplicationScoped
+// public class UserRepository {
 
-    @PersistenceContext
-    EntityManager em;
+//     @PersistenceContext
+//     EntityManager em;
 
-    public void save(User user) {
-        em.persist(user);
-    }
+//     public void save(User user) {
+//         em.persist(user);
+//     }
 
-    public boolean existsByApiKey(String apiKey) {
-        String query = "SELECT COUNT(u) FROM User u WHERE u.apiKey = :key";
-        Long count = em.createQuery(query, Long.class)
-                       .setParameter("key", apiKey)
-                       .getSingleResult();
-        return count > 0;
-    }
+//     public boolean existsByApiKey(String apiKey) {
+//         String query = "SELECT COUNT(u) FROM User u WHERE u.apiKey = :key";
+//         Long count = em.createQuery(query, Long.class)
+//                        .setParameter("key", apiKey)
+//                        .getSingleResult();
+//         return count > 0;
+//     }
 
-    public User findByApiKey(String apiKey) {
-        return em.createQuery(
-        String query = "SELECT u FROM User u WHERE u.apiKey = :key", User.class)
-                 .setParameter("key", apiKey)
-                 .getSingleResult();
-}
-}
+//     public User findByApiKey(String apiKey) {
+//         return em.createQuery(
+//         String query = "SELECT u FROM User u WHERE u.apiKey = :key", User.class)
+//                  .setParameter("key", apiKey)
+//                  .getSingleResult();
+// }
+// }
